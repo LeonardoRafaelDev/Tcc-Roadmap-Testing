8f9f6b8c72a4f8805f48c15ca79262832fa4db6f
ileonardohe4rt <leonardorafaeldev@gmail.com>
2022-10-08 13:33:21 -0300

    added styles tailwind and animation

--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ btn.addEventListener("click", () => {
   window.scrollTo({ top: 0 });
   document.querySelector('body').classList.toggle('overflow-y-hidden')
   menu.classList.toggle("hidden");
-  nav.querySelector("div").classList.toggle('bg-white')
-  nav.querySelector("div").classList.toggle('dark:bg-black')
+  nav.querySelector("div").classList.toggle('bg-black-900')
   nav.classList.toggle('h-screen')
-  nav.classList.toggle('bg-white')
-  btn.querySelector('svg').classList.toggle('text-black')
-  btn.querySelector('svg').classList.toggle('dark:text-white')
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,53 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Inter': ['Inter', 'sans-serif'],
+      }
+    },
+    colors:{
+      primary:{
+        DEFAULT: '#301556',
+        900: '#301556f0',
+      },
+      white:{
+        Default: '#ffffff',
+      },
+      black:{
+        Default: '#494949',
+        900:'#000000',
+      },
+      yellow:{
+        Default: '#FFE249',
+      },
+      green:{
+        Default: '#65B849',
+      },
+      purpleLight:{
+        Default: '#777BB3',
+      },
+      pinkLight:{
+        Default: '#B6AABC',
+      },
+      background:{
+        Default: '#464b58b3',
+        Gradient1: '#301556',
+        Gradient2: '#301556f0',
+        Transparent1: 'ffffff1a',
+        Transparent2: 'ffffff0d',
+        white: '#ffffff',
+      },
+      divider:{
+        Default: '#572A96',
+      },
+      description:{
+        Default: '#858A9B',
+      },
+      Text:{
+        Default: '#FFFFFF',
+      }
+    }
   },
   plugins: [],
 }
